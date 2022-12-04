2661f934b4329d988dc2fcc4eee75d80eba4e08a
martijnkunstman <martijn@kunstman.nl>
2022-12-04 23:04:50 +0100

    day 2

--- day2/script.js
+++ day2/script.js
@@ -1,16 +1,24 @@
 /*
 
+Solution without if of switch statements...
+
 A = X = 0
 B = Y = 1
 C = Z = 2
 
-part1
+a = ABC
+b = XYZ
+c = b * 3
+
+part1 matrix : (((b * 4) + ((4 - a) * 3)) % 9) + 1;
+
 A B C
 3 7 2 X
 0 4 8 Y
 6 1 5 Z
 
-part2
+part2 matrix : ((a + b + 2) % 3) + 1 + c;
+
 A B C
 2 0 1 X
 0 1 2 Y
@@ -19,18 +27,14 @@ A B C
 */
 fetch('./input.txt').then(r => r.text()).then(
     (d) => {
-        d = d.split(/\n/g).map(x => x = x.split(" "));
         let t1 = 0, t2 = 0;
+        d = d.split(/\n/g).map(x => x = x.split(" "));
         d.map(x => {
-            let a = x[0].charAt(0).charCodeAt(0) - 65;
-            let b = x[1].charAt(0).charCodeAt(0) - (65 + 23);
-            let c = b * 3;
-            t1 += (((b * 4) + ((4 - a) * 3)) % 9) + 1;
-            t2 += ((a + b + 2) % 3) + 1 + c;
+            t1 += ((((x[1].charAt(0).charCodeAt(0) - (65 + 23)) * 4) + ((4 - (x[0].charAt(0).charCodeAt(0) - 65)) * 3)) % 9) + 1;
+            t2 += (((x[0].charAt(0).charCodeAt(0) - 65) + (x[1].charAt(0).charCodeAt(0) - (65 + 23)) + 2) % 3) + 1 + ((x[1].charAt(0).charCodeAt(0) - (65 + 23))*3);
         }
         );
-        console.log("part1:" + t1);
-        console.log("part2:" + t2);
+        console.log("part1:" + t1 + " part2:" + t2);
         document.body.innerHTML += t1 + "<br>" + t2;
     }
 )
--- day3/script.js
+++ day3/script.js
@@ -2,26 +2,22 @@ fetch('./input.txt').then(r => r.text()).then(
     (d) => {
         d = d.split(/\n/g);
         let scorepart1 = 0, scorepart2 = 0, tempArray;
-        //part 1 - short  
-        d.map(
-            (s) => {
-                let letter = s.split("").filter((item, index, array) => (array.indexOf(item) != index) && (index >= array.length / 2) && (array.indexOf(item) < array.length / 2))[0];
-                scorepart1 += letter.charAt(0).charCodeAt(0) - 96 < 0 ? letter.charAt(0).charCodeAt(0) - 38 : letter.charAt(0).charCodeAt(0) - 96;
-            }
-        )
-        //part 2 - quick and dirty - using _underscore to find intersection (cheating?)
         d.map(
             (s, i) => {
+                //part 1
+                let letter = s.split("").filter((item, index, array) => (array.indexOf(item) != index) && (index >= array.length / 2) && (array.indexOf(item) < array.length / 2))[0];
+                scorepart1 += letter.charAt(0).charCodeAt(0) - 96 < 0 ? letter.charAt(0).charCodeAt(0) - 38 : letter.charAt(0).charCodeAt(0) - 96;
+                //part 2
                 if (!(i % 3)) {
                     tempArray = [];
                 }
                 tempArray.push(s);
                 if (tempArray.length == 3) { 
-                    let letter = _.intersection(tempArray[0].split(""), tempArray[1].split(""), tempArray[2].split(""))[0];
+                    letter = _.intersection(tempArray[0].split(""), tempArray[1].split(""), tempArray[2].split(""))[0];
                     scorepart2 += letter.charAt(0).charCodeAt(0) - 96 < 0 ? letter.charAt(0).charCodeAt(0) - 38 : letter.charAt(0).charCodeAt(0) - 96;
-                }
+                }            
             }
-        );
+        )
         console.log(scorepart1);
         console.log(scorepart2);
         document.body.innerHTML += scorepart1 + "<br>" + scorepart2;
