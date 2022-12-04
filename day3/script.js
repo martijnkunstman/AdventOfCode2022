1f728c6e6b18633952469e6260dc318271cc325c
martijnkunstman <martijn@kunstman.nl>
2022-12-04 18:33:31 +0100

    day 3

--- day3/script.js
+++ day3/script.js
@@ -1,18 +1,29 @@
 fetch('./input.txt').then(r => r.text()).then(
     (d) => {
         d = d.split(/\n/g);
-        scorepart1 = 0;  
-        scorepart2 = 0; 
-        //part 1   
-        d.map(            
-            (s) => {           
-                let letter = s.split("").filter((item, index,array) => (array.indexOf(item) != index)&&(index >= array.length/2)&&(array.indexOf(item) < array.length/2))[0];
-                scorepart1 +=letter.charAt(0).charCodeAt(0)-96 < 0 ? letter.charAt(0).charCodeAt(0) - 38:letter.charAt(0).charCodeAt(0)-96;
+        let scorepart1 = 0, scorepart2 = 0, tempArray;
+        //part 1 - short  
+        d.map(
+            (s) => {
+                let letter = s.split("").filter((item, index, array) => (array.indexOf(item) != index) && (index >= array.length / 2) && (array.indexOf(item) < array.length / 2))[0];
+                scorepart1 += letter.charAt(0).charCodeAt(0) - 96 < 0 ? letter.charAt(0).charCodeAt(0) - 38 : letter.charAt(0).charCodeAt(0) - 96;
             }
         )
-        //part 2
-        //todo....
+        //part 2 - quick and dirty - using _underscore to find intersection (cheating?)
+        d.map(
+            (s, i) => {
+                if (!(i % 3)) {
+                    tempArray = [];
+                }
+                tempArray.push(s);
+                if (tempArray.length == 3) { 
+                    let letter = _.intersection(tempArray[0].split(""), tempArray[1].split(""), tempArray[2].split(""))[0];
+                    scorepart2 += letter.charAt(0).charCodeAt(0) - 96 < 0 ? letter.charAt(0).charCodeAt(0) - 38 : letter.charAt(0).charCodeAt(0) - 96;
+                }
+            }
+        );
         console.log(scorepart1);
+        console.log(scorepart2);
         document.body.innerHTML += scorepart1 + "<br>" + scorepart2;
     }
 )
