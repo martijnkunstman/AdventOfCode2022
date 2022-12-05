395fc4bc9e57e1f1d1f9b836bbb1bf7d478aac1b
martijnkunstman <martijn@kunstman.nl>
2022-12-05 16:31:42 +0100

    cleanup day 2

--- day2/script.js
+++ day2/script.js
@@ -6,18 +6,18 @@ A = X = 0
 B = Y = 1
 C = Z = 2
 
-a = ABC
-b = XYZ
+a = A || B || C
+b = X || Y || Z
 c = b * 3
 
-part1 matrix : (((b * 4) + ((4 - a) * 3)) % 9) + 1;
+part1 matrix : ((b * 4) + ((4 - a) * 3)) % 9 + 1;
 
 A B C
 3 7 2 X
 0 4 8 Y
 6 1 5 Z
 
-part2 matrix : ((a + b + 2) % 3) + 1 + c;
+part2 matrix : (a + b + 2) % 3 + c + 1
 
 A B C
 2 0 1 X
@@ -25,13 +25,16 @@ A B C
 1 2 0 Z
 
 */
+let l2n = (letter, extra = 0) => {
+    return letter.charAt(0).charCodeAt(0) - (65 + extra)
+}
 fetch('./input.txt').then(r => r.text()).then(
-    (d) => {
+    d => {
         let t1 = 0, t2 = 0;
         d = d.split(/\n/g).map(x => x = x.split(" "));
         d.map(x => {
-            t1 += ((((x[1].charAt(0).charCodeAt(0) - (65 + 23)) * 4) + ((4 - (x[0].charAt(0).charCodeAt(0) - 65)) * 3)) % 9) + 1;
-            t2 += (((x[0].charAt(0).charCodeAt(0) - 65) + (x[1].charAt(0).charCodeAt(0) - (65 + 23)) + 2) % 3) + 1 + ((x[1].charAt(0).charCodeAt(0) - (65 + 23))*3);
+            t1 += (l2n(x[1],23) * 4 + (4 - l2n(x[0])) * 3) % 9 + 1;
+            t2 += (l2n(x[0]) + l2n(x[1],23) + 2) % 3 + l2n(x[1],23)*3 +1;
         }
         );
         console.log("part1:" + t1 + " part2:" + t2);
