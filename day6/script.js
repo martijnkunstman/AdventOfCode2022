7129da08518b905f0f52f09793385bc81d00cc9b
martijnkunstman <martijn@kunstman.nl>
2022-12-06 21:28:11 +0100

    day6 cleanup

--- day6/script.js
+++ day6/script.js
@@ -1,27 +1,19 @@
-let result1 = 4;
-let result2 = 14;
+let result1 = 4
+let result2 = 14
 
-function removeDuplicate(string)
-{
-   return string.split('')
-    .filter(function(item, pos, self)
-    {
-      return self.indexOf(item) == pos;
-    }
-   ).join('');
+function removeDuplicate(string) {
+    return string.split('')
+        .filter(function (item, pos, self) {
+            return self.indexOf(item) == pos
+        }
+        ).join('')
 }
 
 fetch('./input.txt').then(r => r.text()).then(
     s => {
-        while (removeDuplicate(s.substring(result1-4, result1)).length != 4)
-        {
-            result1++;
-        }
-        while (removeDuplicate(s.substring(result2-14, result2)).length != 14)
-        {
-            result2++;
-        }
-        console.log(result1 + " - " + result2);
-        document.body.innerHTML += result1 + "<br>" + result2;
+        while (removeDuplicate(s.substring(result1 - 4, result1)).length != 4) { result1++ }
+        while (removeDuplicate(s.substring(result2 - 14, result2)).length != 14) { result2++ }
+        console.log(result1 + " - " + result2)
+        document.body.innerHTML += result1 + "<br>" + result2
     }
 )
